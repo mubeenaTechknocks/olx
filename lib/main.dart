41208aecb41d00c5e94a8cc02510585813d6d334
sangeetha <sangeethababu63@gmail.com>
2020-10-27 18:52:02 +0530

    Readme update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:olx/navigation.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+//import 'package:olx/navigation.dart';
+//import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:olx/Profile.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(0, 48, 52, 1));
+    //FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(0, 48, 52, 1));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'olx',
-      home: Navigation(),
+      home: Profile(),
+      //home: olx/Navigation
     );
   }
 }
