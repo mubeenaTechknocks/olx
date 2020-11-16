65eea8884d6e556b6c8162dcc8a1d46014897f1f
srujana207 <41240100+srujana207@users.noreply.github.com>
2020-11-16 13:37:38 +0530

    Update Properties.dart

--- lib/Properties.dart
+++ lib/Properties.dart
@@ -1,6 +1,8 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:olx/sell.dart';
+
 class Properties extends StatefulWidget {
   @override
   PropertiesScreenState createState() => PropertiesScreenState();
@@ -20,52 +22,51 @@ class PropertiesScreenState extends State<Properties> {
               Container(
                 width: MediaQuery.of(context).size.width,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-                  child: InkWell(
-                    onTap: () => {},
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 20.0),
-                      /*decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                        color: Colors.black12,
-                        width: 1.0,
-                      ))),*/
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(10, 40, 0, 0),
-                            child: Icon(
-                              Icons.arrow_back,
-                              color: Color.fromRGBO(0, 48, 52, 1),
-                              size: 30.0,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20.0, 40.0, 15.0, 0),
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 0.0, horizontal: 10.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    'Properties',
-                                    style: TextStyle(
-                                      fontSize: 23.0,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color.fromRGBO(0, 48, 52, 1),
-                                      letterSpacing: .1,
-                                      wordSpacing: .5,
-                                    ),
-                                  ),
-                                ],
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+                          child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Color.fromRGBO(0, 48, 52, 1),
+                                size: 30.0,
                               ),
+                              onPressed: () => setState(() {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Sell()),
+                                    );
+                                  })),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 25.0, 15.0, 0),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 0.0, horizontal: 10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  ' Properties',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromRGBO(0, 48, 52, 1),
+                                    letterSpacing: .1,
+                                    wordSpacing: .5,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -79,7 +80,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
@@ -127,7 +128,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
@@ -181,7 +182,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
@@ -235,7 +236,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
@@ -289,7 +290,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
@@ -343,7 +344,7 @@ class PropertiesScreenState extends State<Properties> {
               ),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
                   child: InkWell(
                     onTap: () => {},
                     child: Container(
